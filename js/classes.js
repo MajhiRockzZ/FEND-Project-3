33232235b4bf77ded885c2387f057898cbd1c6e3
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-09-20 18:00:09 +0530

    Add Entity class

---
+++ js/classes.js
@@ -0,0 +1,15 @@
+class Entity {
+  constructor() {
+    // Sprite image
+    this.sprite = 'images/';
+    // x pos
+    this.x = 2;
+    // y pos
+    this.y = 5;
+  }
+
+  render() {
+    // Draw player sprite on current x and y coord position
+    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83);
+  }
+}
